952fff628503a78c16a8449b0ba6480cee2f0d5e
RoundSann <TheRoundler@gmail.com>
2025-12-21 21:56:07 +0800

    Auto backup: 2025/12/21 21:56:07

--- scripts/table-wrap.js
+++
@@ -1,8 +0,0 @@
-/* scripts/table-wrap.js */
-// 在文章渲染完成后，自动给 table 加上外层容器
-hexo.extend.filter.register('after_post_render', function(data){
-  // 简单的正则替换：给 <table> 包裹一个 div
-  // 这样我们就可以对这个 div 设置 overflow-x: auto 了
-  data.content = data.content.replace(/<table/g, '<div class="table-container"><table').replace(/<\/table>/g, '</table></div>');
-  return data;
-});
